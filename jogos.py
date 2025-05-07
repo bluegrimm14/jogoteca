ad48e6d1db014333c5dd8fc3e429b1bab8b9d82f
Terry Roger Pereira Santos <terryrogerps@gmail.com>
2025-05-07 18:26:45 -0300

    feat(Jogo): Set advinhacao game as Maintance

--- jogos.py
+++ jogos.py
@@ -6,29 +6,34 @@ def escolha_seu_jogo():
     print('*****************************************\n')
 
     print("Escolha seu Jogo:", end="\n")
-    print(" (1) Adivinhação \n (2) Forca\n")
     i = 0
 
     while i != 1:
-        jogo = input("Qual jogo você quer jogar?:")
+        print(" (1) Adivinhação \n (2) Forca (Manuntenção)! \n (3) Parar de Jogar.")
+        jogo = input("Qual jogo você quer jogar?")
 
         if(jogo.isnumeric()):
             jogo = int(jogo)
             if(jogo == 1):
-                print("Jogar Advinhação!\n")
+                print("\nJogar Advinhação!\n")
                 adivinhacao.jogar()
                 i = 1
                 break
             elif(jogo == 2):
-                print("Jogar Forca!\n")
-                forca.jogar()
+                print("\nEm Manuntenção!\n")
+                # print("Jogar Forca!\n")
+                # forca.jogar()
+                # i = 1
+                # break
+            elif(jogo == 3):
+                print("\nEncerrando Sistema...")
                 i = 1
                 break
             else:
-                print("Este jogo não existe!\n")
+                print("\nEste jogo não existe!\n")
                 jogo = str(jogo)
         else:
-            print("Digite apenas números!")
+            print("\nDigite apenas números!\n")
 
 if(__name__ == "__main__"):
     escolha_seu_jogo()
